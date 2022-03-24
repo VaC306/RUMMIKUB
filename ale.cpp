dcb48839f203f96df24031997fc631425ff95726
AlexRamos233 <102303043+AlexRamos233@users.noreply.github.com>
2022-03-24 14:32:18 +0100

    Update ale.cpp
    el basket es para maricones

--- ale.cpp
+++ ale.cpp
@@ -1,1 +1,44 @@
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{}
+int buscar(const tJugada & jugada, const tFicha & ficha){
+
+int i= 0, indice = -1;
+bool encontrada =false;
+
+
+while ( encontrada != true && i< numfichas +1){
+     if (jugada[i].color = ficha.color && jugada[i].num = ficha.num){
+          indice = i;
+        
+     }
+     i++;
+   
+}
+return indice;
+
+};
+ 
+void eliminaFichas(tSoporte & soporte, const tJugada & jugada){
+    for( int i=0;i<soporte.contador;i++){
+        int pos
+        if  (buscar( jugada, soporte.fichas[i])!= -1){
+            soporte.fichas[i].color = libre;
+            soporte.fichas[i].num = -1;
+        }
+
+
+
+    };
+}
+
+
+
+int nuevaJugada(tSoporte & soporte, tJugada & jugada):
+
+
+
